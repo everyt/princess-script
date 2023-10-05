e558608d6d85b146295171402dafeeea8f991e44
everyt <everytea.time@gmail.com>
2023-10-05 17:21:32 +0900

    Initial commit

---
+++ bin/princess-script.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+const fs =       require('fs');
+const argparse = require('argparse');
+const princess = require('..');
+
+
+//──────────────────────────────────────────────────────────────//
+
+
+const cli = new argparse.ArgumentParser({
+  prog:     'princess-script',
+  add_help:  false
+});
+
+cli.add_argument('-h', '--help', {
+  action: 'help',
+  help: 'show this help message and exit'
+});
+
+
+cli.add_argument('-v', '--version', {
+  action: 'version',
+  version: require('../package.json').version
+});
+
+
+cli.add_argument('-o', '--output', {
+  help: 'output file',
+  nargs: '?',
+  default: '-'
+});
+
+
+cli.add_argument('file', {
+  help: 'file to read, utf-8 encoded without BOM',
+  nargs: '?',
+  default: '-'
+});
+
+
+//──────────────────────────────────────────────────────────────//
+
+
+var options = cli.parse_args();
+
+
+//──────────────────────────────────────────────────────────────//
+
+
+function readFile(filename, encoding, callback) {
+  if (options.file === '-') {
+    let chunks = [];
+    process.stdin.on('data', chunk => chunks.push(chunk));
+    process.stdin.on('end', () => callback(null, Buffer.concat(chunks).toString(encoding))); 
+  } else {
+    fs.readFile(filename, encoding, callback);
+  }
+}
+
+
+function isPrincessScript(input) {
+  return input.toLowerCase().endsWith('.ps');
+}
+
+
+readFile(options.file, 'utf-8', (error, input) => {
+  let output;
+
+  if (error) {
+    if (error.code === 'ENOENT') {
+      console.error('File not found: ' + options.file);
+      process.exit(2);
+    }
+
+    console.error(
+      options.trace && error.stack ||
+      error.message ||
+      String(error)
+    );
+    
+    process.exit(1);
+  }
+
+  if (isPrincessScript(options.file)) {
+    output = princess.linebreaker(input);
+    console.table(output);
+  } else {
+    console.error('Not a PrincessScript file: ' + options.file);
+    process.exit(2);
+  }
+});
