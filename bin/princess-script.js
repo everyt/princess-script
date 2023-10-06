437072d01b09d2d54c3825e2a2d87e4067f5b05e
everyt <everytea.time@gmail.com>
2023-10-06 11:59:23 +0900

    Fix missing param

--- bin/princess-script.js
+++ bin/princess-script.js
@@ -86,7 +86,7 @@ readFile(options.file, 'utf-8', (error, input) => {
   }
 
   if (isPrincessScript(options.file)) {
-    output = princess.loadScript(input);
+    output = princess.loadScript(input, options);
     console.table(output);
   } else {
     console.error('Not a PrincessScript file: ' + options.file);
